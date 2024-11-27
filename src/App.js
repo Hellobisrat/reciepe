cd0b648209faffd3deef3b54eb0615190dbd2b5e
Hellobisrat <100370010+Hellobisrat@users.noreply.github.com>
2024-11-27 09:32:45 -0600

    nav bar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Home from './pages/home'
+import Details from './pages/favorites'
+import Favorites from './pages/favorites';
+import Navbar from './components/navbar';
 function App() {
   return (
-    <div className="App">
+    <div >
+      <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
+        <Navbar/>
+      <Routes>
+        <Route path='/'element={<Home/>}/>
+        <Route path='/details'element={<Details/>}/>
+        <Route path='/recipe-item/:id'element={<Favorites/>}/>
+      </Routes>
+      </div>
+      
    
         
     </div>
